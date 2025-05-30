1fb9035037e0f7499fce6324e40195e485c78a3b
abhirup-m <mukherjeeabhirup44@gmail.com>
2025-05-30 08:21:49 +0530

    some comments

--- src/eigenstateRG.jl
+++ src/eigenstateRG.jl
@@ -1,4 +1,4 @@
-function getWavefunctionRG(initState::Dict{BitVector,Float64}, alphaValues::Vector{Float64}, numSteps::Integer, unitaryOperatorFunction::Function, stateExpansionFunction::Function, sectors::String; maxSize::Int64=0, tolerance=1e-16, RGtolerance=1e-16)
+function getWavefunctionRG(initState::Dict{BitVector,Float64}, alphaValues::Vector{Float64}, numSteps::Integer, unitaryOperatorFunction::Function, stateExpansionFunction::Function, sectors::String; maxSize::Int64=0, RGtolerance::Float64=1e-16)
 
     @assert numSteps ≤ length(alphaValues)
     numEntangled = div(length(collect(keys(initState))[1]), 2)
@@ -9,10 +9,16 @@ function getWavefunctionRG(initState::Dict{BitVector,Float64}, alphaValues::Vect
         newState = stateExpansionFunction(stateFlowArray[end], sectors)
         unitaryOperatorList = unitaryOperatorFunction(alpha, numEntangled, sectors)
         numEntangled = div(length(collect(keys(newState))[1]), 2)
-        mergewith!(+, newState, fetch.([Threads.@spawn ApplyOperator([operator], newState; tolerance=tolerance) for operator in unitaryOperatorList])...)
 
+        # apply unitary evolution operator to generate new terms
+        newTerms = fetch.([Threads.@spawn ApplyOperator([operator], newState) for operator in unitaryOperatorList])
+        mergewith!(+, newState, newTerms...)
+
+        # remove wavefunction coefficients below a threshold
         maxv = maximum(abs.(values(newState)))
         filter!(x -> abs(x[2])/maxv > RGtolerance, newState)
+
+        # retained only maxSize number of coefficients in wavefunction
         if maxSize > 0 && maxSize < length(newState)
             println("Drop ratio ~ ", sum(sort(abs.(values(newState)), rev=true)[maxSize:end] .^ 2) / sum(values(newState) .^ 2))
             newState = Dict(sort(collect(newState), by=x->x|>last|>abs, rev=true)[1:maxSize])
