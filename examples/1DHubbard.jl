c73082133fce28a7d6b28a5cc39b9d8a48aead8f
abhirup-m <mukherjeeabhirup44@gmail.com>
2025-07-17 18:31:20 +0530

    added simple example of iterdiag

--- examples/1DHubbard.jl
+++ examples/1DHubbard.jl
@@ -1,85 +1,23 @@
 #### Iterative diagonalisation solution of the 1D Hubbard model ####
-using Plots, Measures, Dates, ProgressMeter
-include("../src/base.jl")
-include("../src/correlations.jl")
-include("../src/eigen.jl")
-include("../src/iterativeDiag.jl")
-
-function dimerHamiltonian(U, t)
-    hopping = [
-        ("+-", [1, 3], -t),
-        ("+-", [2, 4], -t),
-        ("+-", [3, 1], -t),
-        ("+-", [4, 2], -t),
-    ]
-    correlation = [
-        ("n", [1], -U / 2),
-        ("n", [2], -U / 2),
-        ("nn", [1, 2], U),
-        ("n", [3], -U / 2),
-        ("n", [4], -U / 2),
-        ("nn", [3, 4], U),
-    ]
-    return [hopping; correlation]
-end
+using Plots, Measures, Fermions
 
 
-function dimerAdditionalHamiltonian(U, t, index)
-    upPosition = 2 * index - 1
-    hopping = [
-        ("+-", [upPosition, upPosition - 2], -t),
-        ("+-", [upPosition + 1, upPosition - 1], -t),
-        ("+-", [upPosition - 2, upPosition], -t),
-        ("+-", [upPosition - 1, upPosition + 1], -t),
-    ]
-
-    correlation = [
-        ("n", [upPosition], -U / 2),
-        ("n", [upPosition + 1], -U / 2),
-        ("nn", [upPosition, upPosition + 1], U),
-    ]
-    return [hopping; correlation]
-end
-
-const t = 1.0
-const retainSizeY = 50
-const retainSizeX = 1e-2
-
-function main(numSteps, U)
-    initBasis = BasisStates(4, [1, 2, 3], [-1, 0, 1], x -> true)
-    hamiltonianFamily = [dimerHamiltonian(U, t)]
-    numStatesFamily = Int64[2]
-    for i in 1:numSteps
-        push!(hamiltonianFamily, dimerAdditionalHamiltonian(U, t, 2 + i)
-        )
-        push!(numStatesFamily, 2 + i)
-    end
-
-    eigValData, eigVecData = IterDiag(
-        hamiltonianFamily,
-        initBasis,
-        numStatesFamily,
-        retainSizeY;
-        showProgress=true,
-        totOccCriteria=(o, N) -> N - 1 ≤ o ≤ N + 1,
-        magzCriteria=m -> -2 ≤ m ≤ 2
+function Hubbard1D(
+        U::Float64,
+        sites::Int64,
     )
-    halfFillingSpectrum = [(E, X) for (E, X, numSites) in zip(eigValData, eigVecData, numStatesFamily)
-                           if (numSites, 0) ∈ keys(E)]
-    groundStateInfoAll = [(numSites, 0) for (E, X, numSites) in zip(eigValData, eigVecData, numStatesFamily)
-                           if (numSites, 0) ∈ keys(E)]
-
-    freqArray = collect(range(-6, stop=6, step=0.01))
-    probe = [("-", [1], 1.0)]
-    probeDag = [("+", [1], 1.0)]
-    specfuncFlow, freqArray = IterSpecFunc(groundStateInfoAll, halfFillingSpectrum, probe, probeDag, freqArray, 0.01, ['N', 'Z'])
-    plots = []
-    for y in specfuncFlow
-        push!(plots, plot(freqArray, y))
+    hamiltonian = Tuple{String, Vector{Int64}, Float64}[]
+    for i in 1:(sites-1)
+        push!(hamiltonian, ("nn", [i, i+1], U))
+        push!(hamiltonian, ("+-", [i, i+1], 1.0))
+        push!(hamiltonian, ("+-", [i+1, i], 1.0))
     end
-    p = plot(plots..., thickness_scaling=1.4, linewidth=2, layout=(length(plots), 1), leftmargin=4mm, size=(400, 300 * length(plots)), legend=false)
-    display(p)
-    #=savefig(p, "specfunc.pdf")=#
+    return hamiltonian
 end
 
-main(8, 2.0)
+sites = 30
+maxSize = 3000
+hubbardHamiltonian = Hubbard1D(2.0, sites)
+hamiltonianFamily = MinceHamiltonian(hubbardHamiltonian, collect(2:sites))
+savePaths, resultsDict = IterDiag(hamiltonianFamily, maxSize; correlationDefDict=Dict("nn-$(i)-$(i+1)"=> [("nn", [i, i+1], 1.0)] for i in 1:(sites-1)))
+display(resultsDict)
