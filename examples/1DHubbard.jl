fb42a5b534874e6b5b188f685cb7340712e35b11
abhirup-m <mukherjeeabhirup44@gmail.com>
2025-09-06 10:21:50 +0530

    removed the constraint of atleast two steps

--- examples/1DHubbard.jl
+++ examples/1DHubbard.jl
@@ -1,5 +1,9 @@
 #### Iterative diagonalisation solution of the 1D Hubbard model ####
-using Plots, Measures, Fermions
+using Plots, Measures, LinearAlgebra
+include("../src/base.jl")
+include("../src/correlations.jl")
+include("../src/constants.jl")
+include("../src/iterDiag.jl")
 
 
 function Hubbard1D(
@@ -19,5 +23,13 @@ sites = 10
 maxSize = 300
 hubbardHamiltonian = Hubbard1D(2.0, sites)
 hamiltonianFamily = MinceHamiltonian(hubbardHamiltonian, collect(2:sites))
+results = IterDiag(hamiltonianFamily, maxSize; symmetries=['N'], correlationDefDict=Dict("nn-$(i)-$(i+1)"=> [("nn", [i, i+1], 1.0)] for i in 1:3:(sites-1)), specFuncDefDict=Dict("any" => [("+", [1], 1.0)]))
+display(results)
+
+sites = 5
+maxSize = 300
+hubbardHamiltonian = Hubbard1D(2.0, sites)
+hamiltonianFamily = MinceHamiltonian(hubbardHamiltonian, [10])
+println(length(hamiltonianFamily))
 results = IterDiag(hamiltonianFamily, maxSize; symmetries=['N'], correlationDefDict=Dict("nn-$(i)-$(i+1)"=> [("nn", [i, i+1], 1.0)] for i in 1:3:(sites-1)), specFuncDefDict=Dict("any" => [("+", [1], 1.0)]))
 display(results)
